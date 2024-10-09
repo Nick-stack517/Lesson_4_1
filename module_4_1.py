f8c7dfdc95abfd3862074a7fe412af1055931a36
Nick-stack517 <gsatello@gmail.com>
2024-10-09 13:35:01 +0700

    Add files via upload

---
+++ module_4_1.py
@@ -0,0 +1,11 @@
+from fake_math import divide as school_mathematics
+from true_math import divide as matan
+
+result1 = school_mathematics(69, 3)
+result2 = school_mathematics(3, 0)
+result3 = matan(49, 7)
+result4 = matan(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
